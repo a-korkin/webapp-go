e1cf44e1fb554899a2de42f7b99d5ba6241fe077
a-korkin <a-korkin88@yandex.ru>
2024-12-09 15:18:03 +0300

    feat: docker and init db added

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Simple webapp on golang
+
+### to start
+``` console
+docker compose up -d
+chmod +x ./start.sh && ./start.sh
+```
---
+++ init.sql
@@ -0,0 +1,12 @@
+create table if not exists person
+(
+    id serial primary key,
+    lname varchar(255),
+    fname varchar(255),
+    age integer
+);
+
+create user :DB_USR with password :'DB_PWD';
+grant all privileges on database :DB_NAME to :DB_USR;
+grant all privileges on schema public to :DB_USR;
+grant usage on schema public to :DB_USR;
